147b0cac6c6d46f79afbef9ca7ed49a2e7fdcf84
Shardul1908 <sharduld80@gmail.com>
2022-03-02 12:50:07 +0530

    rfm ready pie chart ready 2

--- client/pages/index.js
+++ client/pages/index.js
@@ -8,26 +8,9 @@ const Index = (props) => {
 
   return (
     <>
-      <div className={styles.navbar_home}>
-
-      </div>
-
-      <div className={styles.analytics}>
-        {/* <div className={styles.analytics_pie}>
-          
-        </div> */}
-        <PieChart shop={shop} />
-        <div className={styles.analytics_nos}>
-          <h3>Total Customers</h3>
-          <h3>Top Customers</h3>
-          <h3>High Customers</h3>
-          <h3>Medium Customers</h3>
-          <h3>Low Customers</h3>
-          <h3>Lost Customers</h3>
-        </div>
-      </div>
+      {/* <PieChart shop={shop} /> */}
+      <CreateSegmentsNavbar shop={shop} />
     </>
-    // <CreateSegmentsNavbar shop={shop} />
   )
 };
 
