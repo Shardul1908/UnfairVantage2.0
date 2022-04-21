90fc5b17dfcc74b80c5b2f959407ce5a8e1a8df2
tejasbele <tejasrbele@gmail.com>
2022-04-21 11:44:15 +0530

    home page design

--- client/pages/index.js
+++ client/pages/index.js
@@ -3,14 +3,47 @@ import React from "react";
 import CreateSegmentsNavbar from "../Components/CreateSegmentsNavbar.js";
 import PieChart from "../Components/PieChart/PieChart.js";
 import styles from "../styles/homepage.module.css";
+import { Button } from "react-bootstrap";
+import { fontWeight } from "@mui/system";
 
 const Index = (props) => {
   const { shop } = props;
 
   return (
     <div className={styles.homepage}>
-      <div className={styles.homepage_title}>
-        <h3>Home Page</h3>
+      <div className={styles.navbar}>
+        <div className={styles.homepage_title}>
+          <h3>Home Page</h3>
+        </div>
+        <Button className={styles.create_segments}>Create Segments</Button>
+        <div className={styles.dropdown}>
+          <label
+            for="cars"
+            style={{ color: "#ffffff", fontWeight: "bold", fontSize: "18px" }}
+          >
+            Category
+          </label>
+          &nbsp;
+          <select name="Categories" id="categories">
+            <option value="volvo">Top Valued Customers</option>
+            <option value="saab">High Valued Customers</option>
+            <option value="mercedes">Medium Valued Customers</option>
+            <option value="audi">Low Valued Customers</option>
+            <option value="audi">Lost Customers</option>
+          </select>
+          &nbsp;&nbsp;
+          <Button
+            style={{
+              backgroundColor: "#fdfdfd",
+              marginBottom: "4px",
+              color: "#000000",
+              fontSize: "11px",
+              border: "none",
+            }}
+          >
+            Show
+          </Button>
+        </div>
       </div>
       <div className={styles.flex_container}>
         <div className={styles.homepage_div_piechart}>
